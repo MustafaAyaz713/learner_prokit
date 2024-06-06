216df172e7f6d550f630600f708b4e9a8347f833
Mustafa Ayaz <115315223+MustafaAyaz713@users.noreply.github.com>
2024-06-06 19:07:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:learner_prokit/Screens/LearnerSignUp.dart';
 import 'package:learner_prokit/Screens/LearnerWalkThrough.dart';
 import 'package:learner_prokit/store/AppStore.dart';
+import 'package:learner_prokit/supabase_client.dart';
 import 'package:learner_prokit/utils/AppTheme.dart';
 import 'package:learner_prokit/utils/LearnerConstant.dart';
 import 'package:learner_prokit/utils/LearnerDataGenerator.dart';
+import 'package:learner_prokit/utils/LearnerStrings.dart';
 import 'package:nb_utils/nb_utils.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 AppStore appStore = AppStore();
 
+
 void main() async {
+  const String supabaseUrl ="https://amigipnjpmqhmkpelhml.supabase.co";
+  const String supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtaWdpcG5qcG1xaG1rcGVsaG1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY3MjIzMTYsImV4cCI6MjAzMjI5ODMxNn0.ZickxubEFZ0FXY0V0uM1WeZGP-RC6mSAky17SruiX6E";
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey
+    
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await initialize(aLocaleLanguageList: languageList());
@@ -25,7 +38,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Observer(
@@ -42,3 +54,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
