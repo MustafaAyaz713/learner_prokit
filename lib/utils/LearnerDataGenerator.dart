22acfea33a93918d19a2f49cd0cecf5e6b221ad2
Mustafa Ayaz <115315223+MustafaAyaz713@users.noreply.github.com>
2024-06-06 19:08:37 +0300

    Update LearnerDataGenerator.dart

--- lib/utils/LearnerDataGenerator.dart
+++ lib/utils/LearnerDataGenerator.dart
@@ -1,9 +1,12 @@
 
 
+import 'package:flutter/material.dart';
 import 'package:learner_prokit/model/LearnerModels.dart';
 import 'package:learner_prokit/utils/LearnerColors.dart';
+import 'package:learner_prokit/utils/LearnerConstant.dart';
 import 'package:learner_prokit/utils/LearnerImages.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:learner_prokit/supabase_client.dart';
 
 List<LearnerWalk> learnerWalkImg() {
   List<LearnerWalk> list = [];
@@ -23,385 +26,122 @@ List<LearnerWalk> learnerWalkImg() {
   return list;
 }
 
-List<LearnerCategoryModel> learnerGetCategories() {
+ Future<List<LearnerCategoryModel>> learnerGetCategories() async {
   List<LearnerCategoryModel> list = [];
-
-  var category = LearnerCategoryModel();
-  category.img = learner_ic_bag;
-  category.name = "Business";
-
-  var category2 = LearnerCategoryModel();
-  category2.img = learner_ic_pencil_scale;
-  category2.name = "Design";
-
-  var category3 = LearnerCategoryModel();
-  category3.img = learner_ic_crome;
-  category3.name = "Economy";
-
-  var category4 = LearnerCategoryModel();
-  category4.img = learner_ic_telecope;
-  category4.name = "Research";
-
-  var category5 = LearnerCategoryModel();
-  category5.img = learner_ic_medal;
-  category5.name = "Polytics";
-
-  var category6 = LearnerCategoryModel();
-  category6.img = learner_ic_crown;
-  category6.name = "Awards";
-
-  var category7 = LearnerCategoryModel();
-  category7.img = learner_ic_flag;
-  category7.name = "Sports";
-
-  var category8 = LearnerCategoryModel();
-  category8.img = learner_ic_cup;
-  category8.name = "Medals";
-
-  list.add(category);
-  list.add(category2);
-  list.add(category3);
-  list.add(category4);
-  list.add(category5);
-  list.add(category6);
-  list.add(category7);
-  list.add(category8);
+  final data = await supabase.from('learnercategorymodel').select();
+    for(var row in data){
+      list.add(new LearnerCategoryModel(row["img"],row["name"]));
+  }
 
   return list;
 }
-
-List<LearnerFeaturedModel> learnerGetFavourites() {
+ Future<List<LearnerFeaturedModel>> LearnerGetFeatured() async {
   List<LearnerFeaturedModel> list = [];
-  LearnerFeaturedModel featured = LearnerFeaturedModel();
-  featured.img = learner_walk_1;
-  featured.name = "Business Management";
-  featured.price = "\$19.99";
-  featured.type = "Management";
-
-  LearnerFeaturedModel featured2 = LearnerFeaturedModel();
-  featured2.img = learner_walk_2;
-  featured2.name = "IT & Cloud Computing";
-  featured2.price = "\$16.99";
-  featured2.strikePrice = "\$20.99";
-  featured2.type = "Computer";
-
-  LearnerFeaturedModel featured5 = LearnerFeaturedModel();
-  featured5.img = learner_walk_3;
-  featured5.name = "Learn How To Play Guitar";
-  featured5.price = "\$16.99";
-  featured5.strikePrice = "\$20.99";
-  featured5.type = "Music";
-
-  LearnerFeaturedModel featured6 = LearnerFeaturedModel();
-  featured6.img = learner_walk_1;
-  featured6.name = "Basics of baking cakes";
-  featured6.price = "\$16.99";
-  featured6.type = "Cooking";
-
-  LearnerFeaturedModel featured3 = LearnerFeaturedModel();
-  featured3.img = learner_walk_3;
-  featured3.name = "Modern Medicines";
-  featured3.price = "\$10.99";
-  featured3.type = "Medicines";
-
-  LearnerFeaturedModel featured4 = LearnerFeaturedModel();
-  featured4.img = learner_walk_1;
-  featured4.name = "Project Management";
-  featured4.price = "\$16.99";
-  featured4.type = "Management";
-
-  list.add(featured);
-  list.add(featured2);
-  list.add(featured5);
-  list.add(featured6);
-  list.add(featured3);
-  list.add(featured4);
+  final data = await supabase.from('learnerfeaturedmodel').select();
+    for(var row in data){
+      list.add(new LearnerFeaturedModel(row["img"],row["name"],row["price"],row["strikeprice"],row["type"]));
+  }
 
   return list;
 }
 
-List<LearnerPeopleModel> learnerGetRecommended() {
+
+Future<List<LearnerPeopleModel>> learnerGetRecommended() async {
   List<LearnerPeopleModel> list = [];
-
-  var people = LearnerPeopleModel();
-  people.img = learner_walk_1;
-  list.add(people);
-
-  var people2 = LearnerPeopleModel();
-  people2.img = learner_ic_Cake;
-  list.add(people2);
-
-  var people3 = LearnerPeopleModel();
-  people3.img = learner_green_image;
-  list.add(people3);
-
-  var people4 = LearnerPeopleModel();
-  people4.img = learner_climb;
-  list.add(people4);
-
-  var people5 = LearnerPeopleModel();
-  people5.img = learner_walk_3;
-  list.add(people5);
-
-  var people6 = LearnerPeopleModel();
-  people6.img = learner_ic_Profile;
-  list.add(people6);
+  final data = await supabase.from('learnerpeoplemodel').select();
+    for(var row in data){
+      list.add(new LearnerPeopleModel(row["img"],row["name"],row["points"],row["isonline"],row["subject"],row["email"],row["email"]));
+  }
 
   return list;
+
+
+
+  
 }
 
-List<LearnerPeopleModel> learnerGetInstructor() {
+Future<List<LearnerPeopleModel>> learnerGetInstructor() async {
   List<LearnerPeopleModel> list = [];
-
-  LearnerPeopleModel people = LearnerPeopleModel();
-  people.img = learner_ic_profile2;
-  people.name = "James";
-  people.subject = "Economy";
-  people.isOnline = true;
-
-  list.add(people);
-
-  LearnerPeopleModel people2 = LearnerPeopleModel();
-  people2.img = learner_ic_profile3;
-  people2.name = "Anna";
-  people2.subject = "Design";
-  list.add(people2);
-
-  LearnerPeopleModel people3 = LearnerPeopleModel();
-  people3.img = learner_ic_profile4;
-  people3.name = "Louisa";
-  people3.subject = "Photography";
-  people3.isOnline = true;
-
-  list.add(people3);
-
-  LearnerPeopleModel people4 = LearnerPeopleModel();
-  people4.img = learner_ic_profile5;
-  people4.name = "Walter";
-  people4.subject = "Artist";
-  list.add(people4);
-
-  LearnerPeopleModel people5 = LearnerPeopleModel();
-  people5.img = learner_ic_profile6;
-  people5.name = "Emma Watson";
-  people5.subject = "Mathematician";
-  people5.isOnline = true;
-
-  list.add(people5);
-
-  LearnerPeopleModel people6 = LearnerPeopleModel();
-  people6.img = learner_ic_profile7;
-  people6.name = "Julie Gonas";
-  people6.subject = "Sports";
-  people6.isOnline = true;
-
-  list.add(people6);
+  final data = await supabase.from('learnerpeoplemodel').select();
+    for(var row in data){
+      list.add(new LearnerPeopleModel(row["img"],row["name"],row["points"],row["isonline"],row["subject"],row["email"],row["email"]));
+  }
 
   return list;
+
 }
 
-List<LearnerBadgeModel> learnerGetBadges() {
+
+  Future<List<LearnerBadgeModel>> learnerGetBadges() async {
   List<LearnerBadgeModel> list = [];
-
-  LearnerBadgeModel badge = LearnerBadgeModel();
-  badge.img = learner_ic_medal;
-  badge.name = "Quick Learner";
-  badge.comment = "Completed 1 course";
-  badge.color = learner_colorPrimary;
-  list.add(badge);
-
-  LearnerBadgeModel badge1 = LearnerBadgeModel();
-  badge1.img = learner_ic_crown;
-  badge1.name = "Master Mind!";
-  badge1.comment = "Got 1st place on leaderboard";
-  badge1.color = learner_light_green;
-  list.add(badge1);
-
-  LearnerBadgeModel badge2 = LearnerBadgeModel();
-  badge2.img = learner_ic_cup;
-  badge2.name = "Super Learner";
-  badge2.comment = "Completed more than 5 courses";
-  badge2.color = learner_light_pink;
-  list.add(badge2);
-
-  LearnerBadgeModel badge3 = LearnerBadgeModel();
-  badge3.img = learner_ic_flag;
-  badge3.name = "The Achiever";
-  badge3.comment = "Logged in everyday for a month";
-  badge3.color = learner_light_red;
-  list.add(badge3);
+  final data = await supabase.from('learnerbadgemodel').select();
+    for(var row in data){
+      list.add(new LearnerBadgeModel(row["img"],row["name"],row["comment"],row["islocked"]));
+  }
 
   return list;
+
 }
 
-List<LearnerPeopleModel> learnerGetPending() {
+
+Future<List<LearnerPeopleModel>> learnerGetPending() async {
   List<LearnerPeopleModel> list = [];
+  final data = await supabase.from('learnerpeoplemodel').select();
+    for(var row in data){
+      list.add(new LearnerPeopleModel(row["img"],row["name"],row["points"],row["isonline"],row["subject"],row["email"],row["email"]));
+  }
 
-  LearnerPeopleModel people = LearnerPeopleModel();
-  people.img = learner_ic_Profile;
-  people.name = "Alice Smith";
-  people.isOnline = true;
-  people.points = "109565 Points";
-  people.email = "alicesmith@gmail.com";
-  people.frd = "12 mutual Friends";
-  list.add(people);
-
-  LearnerPeopleModel people2 = LearnerPeopleModel();
-  people2.img = learner_ic_profile1;
-  people2.name = "Hennah Tran";
-  people2.points = "206598 Points";
-  people2.email = "hennahTran@gmail.com";
-  people2.frd = "3 mutual Friends";
-  list.add(people2);
-
-  LearnerPeopleModel people3 = LearnerPeopleModel();
-  people3.img = learner_ic_profile2;
-  people3.name = "Louisa MacGee";
-  people3.points = "896531 Points";
-  people3.email = "Louisamacgee@gmail.com";
-  people3.frd = "7 mutual Friends";
-  list.add(people3);
-
-  LearnerPeopleModel people4 = LearnerPeopleModel();
-  people4.img = learner_ic_profile3;
-  people4.name = "Walter James";
-  people4.points = "106532 Points";
-  people4.email = "WalterJames@gmail.com";
-  people4.frd = "6 mutual Friends";
-  people4.isOnline = true;
-  list.add(people4);
-
-  LearnerPeopleModel people5 = LearnerPeopleModel();
-  people5.img = learner_ic_profile4;
-  people5.name = "Nia Scott";
-  people5.points = "236585 Points";
-  people5.email = "niaScott@gmail.com";
-  people5.frd = "16 mutual Friends";
-  list.add(people5);
-
-  LearnerPeopleModel people6 = LearnerPeopleModel();
-  people6.img = learner_ic_profile5;
-  people6.name = "Hennah Tran";
-  people6.points = "569432 Points";
-  people6.email = "hennahTran@gmail.com";
-  people6.frd = "12 mutual Friends";
-  list.add(people6);
   return list;
+
 }
 
-List<LearnerCoursesModel> learnerGetCourses() {
+
+  
+Future<List<LearnerCoursesModel>> learnerGetCourses() async {
   List<LearnerCoursesModel> list = [];
-  LearnerCoursesModel courses = LearnerCoursesModel();
-  courses.img = learner_walk_1;
-  courses.name = "Business Management";
+  final data = await supabase.from('learnercoursesmodel').select();
+    for(var row in data){
+      list.add(new LearnerCoursesModel(row["img"],row["name"]));
+  }
 
-  LearnerCoursesModel courses2 = LearnerCoursesModel();
-  courses2.img = learner_walk_2;
-  courses2.name = "It & Cloud Computing";
-
-  LearnerCoursesModel courses3 = LearnerCoursesModel();
-  courses3.img = learner_walk_3;
-  courses3.name = "Learn How To Play Guitar";
-
-  LearnerCoursesModel courses4 = LearnerCoursesModel();
-  courses4.img = learner_walk_1;
-  courses4.name = "Basic of baking cakes";
-
-  LearnerCoursesModel courses5 = LearnerCoursesModel();
-  courses5.img = learner_walk_2;
-  courses5.name = "Modren Medicines";
-
-  LearnerCoursesModel courses6 = LearnerCoursesModel();
-  courses6.img = learner_walk_3;
-  courses6.name = "Project Management";
-
-  list.add(courses);
-  list.add(courses2);
-  list.add(courses3);
-  list.add(courses4);
-  list.add(courses5);
-  list.add(courses6);
   return list;
+
 }
 
-List<LearnerContentModel> learnerGetContents() {
+Future<List<LearnerContentModel>> learnerGetContents() async {
   List<LearnerContentModel> list = [];
-  var content = LearnerContentModel();
-  content.name = "Introduction";
-  content.subtitle = "About this course and overview";
-  content.type = "Video";
-  list.add(content);
+  final data = await supabase.from('learnercontentmodel').select();
+    for(var row in data){
+      list.add(new LearnerContentModel(row["name"],row["subtitle"],row["type"]));
+  }
 
-  var content2 = LearnerContentModel();
-  content2.name = "What is management?";
-  content2.subtitle = "Basics and introduction";
-  content2.type = "Article";
-  list.add(content2);
-
-  var content3 = LearnerContentModel();
-  content3.name = "How does your decision effect your project?";
-  content3.subtitle = "Learn the  decision effect";
-  content3.type = "Presentation";
-  list.add(content3);
   return list;
+
 }
 
-List<LearnerChatModel> learnerGetChatData() {
+Future<List<LearnerChatModel>> learnerGetChatData() async {
   List<LearnerChatModel> list = [];
-  var content1 = LearnerChatModel();
-  content1.name = "Alice Smith";
-  content1.msg = "Hi Alice How are you";
-  content1.img = learner_ic_profile3;
-  content1.isOnline = true;
-  list.add(content1);
-
-  var content2 = LearnerChatModel();
-  content2.name = "Hennah Tran";
-  content2.msg = "Hi can u explain me this topic?";
-  content2.img = learner_ic_profile4;
-  list.add(content2);
-
-  var content3 = LearnerChatModel();
-  content3.name = "Hennah Tran";
-  content3.msg = "Hi can u explain me this topic?";
-  content3.img = learner_ic_profile6;
-  list.add(content3);
-
-  var content4 = LearnerChatModel();
-  content4.name = "Hennah Tran";
-  content4.msg = "Hi can u explain me this topic?";
-  content4.img = learner_ic_profile2;
-  content4.isOnline = true;
-  list.add(content4);
+  final data = await supabase.from('learnerchatmodel').select();
+    for(var row in data){
+      list.add(new LearnerChatModel(row["name"],row["msg"],row["img"],row["isonline"]));
+  }
 
   return list;
+
 }
 
-List<LearnerLectureModel> learnerGetLectureData() {
+
+Future<List<LearnerLectureModel>> learnerGetLectureData() async {
   List<LearnerLectureModel> list = [];
-  var content1 = LearnerLectureModel();
-  content1.no = "1";
-  content1.title = "Introducation";
-  content1.subtitle = "About this course and overview.";
-  content1.type = "video";
-  list.add(content1);
-
-  var content2 = LearnerLectureModel();
-  content2.no = "2";
-  content2.title = "What is management?";
-  content2.subtitle = "Basics and introducation";
-  content2.type = "Article";
-  list.add(content2);
-
-  var content3 = LearnerLectureModel();
-  content3.no = "3";
-  content3.title = "How does your decision effect your project?";
-  content3.subtitle = "Learner the most importanted asp..";
-  content3.type = "video";
-  list.add(content3);
+  final data = await supabase.from('learnerlecturemodel').select();
+    for(var row in data){
+      list.add(new LearnerLectureModel(row["no"],row["title"],row["subtitle"],row["type"]));
+  }
 
   return list;
+
 }
+
 
 List<ChatModel> getMessages() {
   List<ChatModel> list = [];
