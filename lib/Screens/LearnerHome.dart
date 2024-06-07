8a4a2e985e02b0085b898607ff6962dae329daea
Halil Köse <108081901+halilkoseh@users.noreply.github.com>
2024-06-07 10:02:24 +0300

    Update LearnerHome.dart

--- lib/Screens/LearnerHome.dart
+++ lib/Screens/LearnerHome.dart
@@ -1,163 +1,81 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/material.dart';
-import 'package:learner_prokit/model/LearnerModels.dart';
-import 'package:learner_prokit/utils/AppWidget.dart';
-import 'package:learner_prokit/utils/LearnerColors.dart';
-import 'package:learner_prokit/utils/LearnerConstant.dart';
-import 'package:learner_prokit/utils/LearnerDataGenerator.dart';
-import 'package:learner_prokit/utils/LearnerStrings.dart';
-import 'package:nb_utils/nb_utils.dart';
+const learner_sample_long_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+const learner_Get_Started = "Giriş Yap";
+const learner_hint_email = "Email";
+const learner_hint_password = "Şifre";
+const learner_lbl_enter = "Giriş Yap";
+const learner_sign_up_info = "Katıla basarak bu şartları kabul edersiniz";
 
-  class LearnerHome extends StatefulWidget {
-     @override
-_LearnerHomeState createState() => _LearnerHomeState();
-}
+const learner_hint_full_name = "Kullanıcı Adı ";
+const learner_lbl_login_underlined = "<u>Giriş Yap</u>";
+const learner_lbl_join = "Katıl";
+const learner_lbl_don_t_joined_yet = "Henüz kayıt olmadın mı !";
+const learner_txt_terms_condition = "Terms & Condition";
 
-class _LearnerHomeState extends State<LearnerHome> {
-late List<LearnerFeaturedModel> mList1 = [];
-late List<LearnerCategoryModel> mList2 = [];
+const learner_lbl_featured = "Eğitim Yerleşkemiz";
+const learner_lbl_see_all = "";
+const learner_lbl_categories = "Eğitim Branşlarımız";
+const learner_hint_search = "Ara...";
+const learner_lbl_filter = "Filtre";
+const learner_lbl_top_instructors = "En İyi Eğitmenler";
+const learner_lbl_recommended = "Önerilenler";
 
- @override
- void initState() {
-  super.initState();
-  loadData();
- }
+String? kullanici_adi = " ";
+const learner_display_name = "";
 
- void loadData() async {
-  mList1 = await LearnerGetFeatured();
-  mList2 = await learnerGetCategories();
-  setState(() {});
- }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(learner_lbl_featured, style: boldTextStyle()),
-                  Text(learner_lbl_see_all, style: boldTextStyle(color: learner_colorPrimary)),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.center,
-              height: 270,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: mList1.length,
-                shrinkWrap: true,
-                padding: EdgeInsets.only(right: 16),
-                itemBuilder: (context, index) {
-                  return LearnerFeatured(mList1[index], index);
-                },
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 16),
-              child: text(learner_lbl_categories, fontFamily: fontBold, fontSize: textSizeNormal),
-            ),
-            16.height,
-            GridView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: mList2.length,
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return LearnerCategory(mList2[index], index);
-              },
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-              ),
-            ),
-            16.height,
-          ],
-        ),
-      ),
-    );
-  }
-}
+const learner_lbl_390_290_points = "390,290 İlerleme Puanı";
+const learner_lbl_general = "Genel";
+const learner_lbl_favourite_courses = "Favori Derslerim";
+const learner_lbl_my_friends = "Arkadaşlarım";
+const learner_lbl_achievements = "Ödüller";
+const learner_lbl_settings = "Ayarlar";
+const learner_lbl_edit_login_details = "Hesap Hakkında";
+const learner_lbl_update_interests = "İlgi Alanlarım";
+const learner_lbl_blocked_users = "Engellenen Kullanıcılar";
 
-class LearnerFeatured extends StatelessWidget {
-  final LearnerFeaturedModel model;
+const learner_lbl_favourites = "Favourites";
+const learner_lbl_locked_badge = "Locked Badge";
+const learner_lbl_unlock_to_see_details = "Unlock to see details";
+const learner_lbl_leader_board = "Leaderboard";
+const learner_lbl_badges = "Badges";
+const learner_lbl_My_Friends = "My Friends";
+const learner_lbl_Accepted = "Accepted";
+const learner_lbl_pending = "Pending";
 
-  LearnerFeatured(this.model, int pos);
+const learner_lbl_Student = "Bilgi Yüklenemiyor";
+const learner_lbl_Course = "KURSLAR";
+const learner_lbl_Points = "POINTS";
+const learner_lbl_Ranks = "RANKS";
 
-  @override
-  Widget build(BuildContext context) {
-    var w = MediaQuery.of(context).size.width;
+const learner_lbl_My_Courses = "My Courses";
+const learner_lbl_All = "All";
+const learner_lbl_Ongoing = "Ongoing";
+const learner_lbl_Completed = "Completed";
+const learner_lbl_project_management = "Project Management";
+const learner_lbl_30_80 = "\$30.80";
+const learner_lbl_1_6k = "1.6K";
+const learner_lbl_students = "Students";
+const learner_lbl_rating = "Rating";
+const learner_lbl_37 = "37";
+const learner_lbl_lectures = "Lectures";
+const learner_lbl_5_0 = "5.0";
+const learner_lbl_swipe_up_to_show_course_contents = "Swipe up to show course contents";
+const learner_lbl_hannah_tran = "Hannah Tran";
 
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.35,
-      margin: EdgeInsets.only(left: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: CachedNetworkImage(
-              placeholder: (context, url) => CircularProgressIndicator(),
-              errorWidget: (context, url, error) => Icon(Icons.error),
-              imageUrl: model.img,
-              fit: BoxFit.cover,
-              height: w * 0.4,
-              width: MediaQuery.of(context).size.width,
-            ),
-          ),
-          SizedBox(height: 4),
-          Padding(
-            padding: EdgeInsets.only(left: 4, right: 4),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(model.name, style: boldTextStyle()),
-                4.height,
-                Text(model.price, style: primaryTextStyle(color: learner_textColorSecondary)),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class LearnerCategory extends StatelessWidget {
-  final LearnerCategoryModel model;
-
-  LearnerCategory(this.model, int pos);
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width,
-          alignment: Alignment.center,
-          margin: EdgeInsets.only(left: 16, right: 16),
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(8),
-                decoration: boxDecoration(bgColor: context.cardColor, radius: 12),
-                child: Image.asset(
-                  model.img,
-                  height: context.width() * 0.17,
-                  width: context.width(),
-                ),
-              ),
-              8.height,
-              Text(model.name, style: boldTextStyle()),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
+const learner_lbl_modren_medicine = "Modren Medicine";
+const learner_lbl_Louisa_macGee = "Louisa macGee";
+const learner_lbl_Lessons = "Lessons";
+const learner_lbl_Reviews = "Reviews";
+const learner_lbl_About = "About";
+const learner_lbl_chat = "Chats";
+const learner_lbl_Instrctors = "Instructors";
+const learner_lbl_Friends = "Friends";
+const learner_lbl_Bots = "Bots";
+const learner_lbl_Start_a_new_chat = "Sohbet Başlat";
+const learner_lbl_start = "Start";
+const learner_lbl_Follow = "Takip Et";
+const learner_lbl_Ratting = "PUAN";
+const learner_lbl_Stud = "ÖĞRENCİLER";
+const learner_lbl_Introducation = "Introducation";
+const learner_lbl_Article = "Article";
+const learner_lbl_2K_Student = "2.8K STUNDENTS";
+const learner_36_Lectures = "36 LECTURES";
